369d600ef686c2f9b803cb551dbd07f38284b267
svn <svn@b387b25f-291c-0410-8dc7-e51caacd5b47>
2007-04-19 20:19:05 +0000

    auto checkin

--- c/feed/rv.c
+++ c/feed/rv.c
@@ -1,41 +1,42 @@
-//2006.03.15  req[c;subject;dict]
-//w32>cl /LD rv.def ..\c\rv.c q.lib ws2_32.lib tibrv.lib
-//l32>gcc -shared ../c/rv.c -o rv.so -I/dat/tibrv/linux/7.2/include -L/dat/tibrv/linux/7.2/lib -ltibrv 
-//l64>gcc -shared ../c/rv.c -o rv.so -I/dat/tibrv/linux/7.3/include -L/dat/tibrv/linux/7.3/lib -ltibrv64 -fPIC 
-//s32>gcc -G ../c/rv.c -o rv.so -ltibrv
-#include"k.h"
-#include"tibrv/tibrv.h"  //missing KC KM KD KU KV KT  3,8,9 14..25  (22-none) 1msg 7opaque 26port 27addr 32encr 47xml
-#define Q(x) P(s=x,krr((S)tibrvStatus_GetText(s)))
-ZI tr(I t){R t==24?8:t/2-3;}
-ZI rt[]={0,9,0,0,14,16,18,20,24,25,8,8,0,0,0, 3};Z tibrvMsgField f;Z tibrv_status s;ZJ j9=(J)1e9;
-ZI nt[]={0,4,0,0, 1, 2, 4, 8, 4, 8,0,0,0,0,0,12};ZS sr(S s){C b[4];R ss(s&&*s?s:(sprintf(b,"%d",f.id),b));}
-
-ZK qf(){K x;I t=f.type;R t==3?kz((j9*f.data.date.sec+f.data.date.nsec)/8.64e13-10957):
- t==8?kp((S)f.data.str):t==9?kb(f.data.boolean):t<14||t>25&&t<34||t>45||t==22?ktn(0,0):
+//2007.05.19 req(inbox) tables and lists
+//w32>cl /LD rv.def ..\c\feed\rv.c q.lib ws2_32.lib tibrv.lib
+//l32>gcc -shared ../c/feed/rv.c -o rv.so -I/dat/tibrv/linux/7.2/include -L/dat/tibrv/linux/7.2/lib -ltibrv 
+//l64>gcc -shared ../c/feed/rv.c -o rv.so -I/dat/tibrv/linux/7.3/include -L/dat/tibrv/linux/7.3/lib -ltibrv64 -fPIC 
+//s32>/usr/local/gcc-3.3.2/bin/gcc -G ../c/feed/rv.c -o rv.so -I/dat/tibrv/7.5.2/include -L/dat/tibrv/7.5.2/lib -ltibrv
+//s64>/usr/local/gcc-3.3.2/bin/gcc -G -m64 ../c/feed/rv.c -o rv.so -I/dat/tibrv/7.5.2/include -L/dat/tibrv/7.5.2/lib -ltibrv64
+#include"../c/k.h"
+#include"tibrv/tibrv.h"  //missing KC KM KD KU KV KT  3z,8s,9b 14..25  (22-none) 1msg 7opaque 26port 27addr 32encr 47xml
+extern V*malloc(),free();extern I memcpy(),strlen();
+ZI tr(I t){R t==24?8:t/2-3;}ZJ o=10957*86400,j9=(J)1e9;Z tibrvMsgField f;Z tibrv_status s;
+ZI rt[]={0,9,0,0,14,16,18,20,24,25,8,8,0,0,0, 3};typedef tibrvMsg M;ZC inbox[99];
+ZI nt[]={0,4,0,0, 1, 2, 4, 8, 4, 8,0,0,0,0,0,12};ZV md(M m){tibrvMsg_Destroy(m);}
+ZK qf(){K x;I t=f.type;R t==3?kz((j9*(f.data.date.sec-o)+f.data.date.nsec)/8.64e13):
+ t==8?kp((S)f.data.str):t==9?kb(f.data.boolean):t<14||(t>25&&t<34)||t>45||t==22?(x=ka(101),xi=0,x):
  t<26?(x=ka(-tr(t)),xj=f.data.i64,x):(x=ktn(tr(t-20),f.count),memcpy(xG,f.data.array,xn*f.size),x);}
-ZK qm(tibrvMsg m){K x,y;I n;tibrvMsg_GetNumFields(m,&n),x=ktn(KS,n),y=ktn(0,n);
- DO(n,tibrvMsg_GetFieldByIndex(m,&f,i);xS[i]=sr((S)f.name);kK(y)[i]=f.type==1?qm(f.data.msg):qf())R xD(x,y);}
-ZI fq(K x){ZS s;I a=xt<0,t=a?-xt:xt;J j;U(t<=KZ)f.count=1,f.type=rt[t],f.size=nt[t];SW(xt){
- CS(KC,if(s)free(s);memcpy(f.data.str=s=(S)malloc(f.size=xn+1),xG,xn);s[xn]=0)CS(-KB,f.data.boolean=xg)
- CS(-KS,f.size=strlen(f.data.str=xs)+1)CS(-KZ,j=.5+8.64e13*(10957+xf);f.data.date.sec=j/j9;f.data.date.nsec=j%j9)
- CD:if(a)f.data.i64=xj;else f.count=xn,f.type+=20,f.data.array=xG;}R t;}
-Z tibrvMsg mq(K x){K y=xK[1],z;tibrvMsg m,s;tibrvMsg_Create(&m),x=*xK;DO(xn,z=kK(y)[i];if(z->t==XD){
- tibrvMsg_AddMsg(m,xS[i],s=mq(z));tibrvMsg_Destroy(s);}else{f.name=xS[i];P(!fq(z),0)tibrvMsg_AddField(m,&f);})R m;}
-
-ZV u(tibrvEvent e,tibrvMsg m,V*c){S s;tibrvMsg_GetSendSubject(m,(const char**)&s),r0(k(0,"upd",ks(ss(s)),qm(m),(K)0));}
-K sub(K x,K y){tibrvEvent e;P(y->t!=-KS,krr("type"))Q(tibrvEvent_CreateListener(&e,TIBRV_DEFAULT_QUEUE,u,xi,y->s,NULL))R r1(y);}
-K req(K x,K y,K z){tibrvMsg m,r;P(y->t!=-KS||z->t!=XD||kK(z)[1]->t||!(m=mq(z)),krr("type"))tibrvMsg_SetSendSubject(m,y->s);
- Q(xi<0?tibrvTransport_Send(-xi,m):tibrvTransport_SendRequest(xi,m,&r,-1))tibrvMsg_Destroy(m);P(xi<0,r1(y))R y=qm(r),tibrvMsg_Destroy(r),y;}
-K pub(K x,K y,K z){R y=req(x=ki(-xi),y,z),r0(x),y;}
+ZV fq(K x){ZS s;I a=xt<0,t=a?-xt:xt;J j;f.count=1,f.size=nt[t],f.type=rt[t];SW(xt){CS(-KB,f.data.boolean=xg)
+ CS(-KZ,j=8.64e13*xf;f.data.date.sec=j/j9+o;f.data.date.nsec=j%j9)CS(-KS,f.size=strlen(f.data.str=xs)+1)
+ CS(KC,if(s)free(s);f.data.str=s=(S)malloc(f.size=xn+1);memcpy(s,xG,xn);s[xn]=0)
+ CD:if(a)f.data.i64=xj;else f.count=xn,f.type+=20,f.data.array=xG;}}
+Z K qm(M m){S s;K x,y;I n;tibrvMsg_GetNumFields(m,&n),x=ktn(KS,n),y=ktn(0,n);
+ DO(n,tibrvMsg_GetFieldByIndex(m,&f,i);s=(S)f.name;xS[i]=s?ss(s):0;kK(y)[i]=f.type==1?qm(f.data.msg):qf(f))
+ R y=k(0,"k)::'",y,(K)0),xn&&*xS?xD(x,y):(r0(x),y);}
+Z M mq(K x){S s;K y,z;I j;M m,t;tibrvMsg_Create(&m);if(y=xt==XD?*xK:0)x=xK[1];if(r1(x),j=0<xt)x=k(0,"k){*:'(::),(;0)'x}",x,(K)0);if(!xt)
+ DO(xn-j,z=xK[i+j];s=y?kS(y)[i]:0;if(z->t==XD){tibrvMsg_AddMsg(m,s,t=mq(z));md(t);}else{f.name=s;fq(z);tibrvMsg_AddField(m,&f);})R r0(x),m;}
+ZK snd(I i,S s,K x){M m=mq(x),r;tibrvMsg_SetSendSubject(m,s);P(i<0,(tibrvTransport_Send(-i,m),md(m),(K)0))
+ R tibrvMsg_SetReplySubject(m,inbox),tibrvTransport_SendRequest(i,m,&r,-1),md(m),x=qm(r),md(r),x;}
+ZV u(tibrvEvent e,M m,V*c){S s,r;K x;tibrvMsg_GetSendSubject(m,(const char**)&s),tibrvMsg_GetReplySubject(m,(const char**)&r);
+ x=k(0,r?"rep":"upd",ks(ss(s)),qm(m),(K)0);if(xt==-128)O("error: %s\n",xs);if(r)snd(-(I)c,r,x);r0(x);}
+#define Q(x) P((s=x),krr((S)tibrvStatus_GetText(s)))
+ZK ch(K x,K y){R xt!=-KI||y->t!=-KS?krr("type"):y;}
+K sub(K x,K y){tibrvEvent e;U(ch(x,y))Q(tibrvEvent_CreateListener(&e,TIBRV_DEFAULT_QUEUE,u,xi,y->s,(V*)xi))R r1(y);}
+K req(K x,K y,K z){U(ch(x,y))R snd(xi,y->s,z);}
 #ifdef WIN32 //pipe(I*p){R CreatePipe(p+0,p+1,0,0),O("%d\n",*p);}
 ZV u0(tibrvQueue q,V*c){tibrvQueue_Dispatch(TIBRV_DEFAULT_QUEUE);}ZV u2(){}
 #else
 #include<unistd.h>
-ZI p[2];ZC b;ZK u1(I i){R read(i,&b,1),tibrvQueue_Dispatch(TIBRV_DEFAULT_QUEUE),ktn(0,0);}
+ZI p[2];ZC b;ZK u1(I i){read(i,&b,1),tibrvQueue_Dispatch(TIBRV_DEFAULT_QUEUE);R 0;}
 ZV u0(tibrvQueue q,V*c){write(p[1],&b,1);}ZV u2(){pipe(p),sd1(p[0],u1);}
-#endif
-// service/network/daemon
-K con(K x){Z i;I c;if(!i){Q(tibrv_Open())Q(tibrvQueue_SetHook(TIBRV_DEFAULT_QUEUE,u0,0))i=1,u2();}
- Q(tibrvTransport_Create(&c,*xS,xS[1],xS[2]))R ki(c);}
-K dis(K x){Q(tibrvTransport_Destroy(xi))R ktn(0,0);}
-//  tibrvEvent_Destroy(response_id);
+#endif // service/network/daemon
+K con(K x){ZI i;I c;if(!i){Q(tibrv_Open())Q(tibrvQueue_SetHook(TIBRV_DEFAULT_QUEUE,u0,0))i=1,u2();}
+ Q(tibrvTransport_Create(&c,*xS,xS[1],xS[2]))tibrvTransport_CreateInbox(c,inbox,99);R ki(c);}
+K dis(K x){Q(tibrvTransport_Destroy(xi))R 0;}
--- c/feed/rv.q
+++ c/feed/rv.q
@@ -1,29 +1,23 @@
-upd:{[t;x]0N!(t;x)}
 con:`rv 2:(`con;1)
 sub:`rv 2:(`sub;2)
-pub:`rv 2:(`pub;3)
 req:`rv 2:(`req;3)
-s:`asd
-d:`a`b`g`h`i`j`e`f`s`z!("asdf";1b;0x23;23h;23;23j;2.3e;2.3;`qwe;2000.01.02T12:34:56.789)
-c:con```
-sub[c;s];
-d:`a`b!("asdf";`u`i!(2 3;3.4))
-pub[c;s;d];
+pub:{req[neg x;y;z]}
+
 \
 
-n:10000
-i:0
-upd:{y;if[n=i+:1;0N!.z.z]}
-k)d:(,`a)!,100#"asdf"
-0N!.z.z;do[n;pub[c;t]d]
-h:hopen 1
-\t do[n;(neg h)d];h""
-\
-t:`NASDAQL1.AAPL / test
+c:con```;s:`asd
 
-c:con(`7523;`$"eth0;239.5.5.135;239.5.5.136";`drumset:7500)
-sub[c]t
 
-t:`CMTE.DATA.TEST.SERVICE.GETALL  LISTALL
-c:con(`8425;`$";239.191.84.25";`tcp:7500)
-req[c;t;(enlist `SERVICE_NAME)!enlist"QTEST"]
+/server
+upd:{0N!(x;y)};rep:{y};sub[c;s]
+
+
+/client
+d:`b`g`h`i`j`e`f`s`z!(1b;0x23;23h;23;23j;2.3e;2.3;"qwe";2000.01.02T12:34:56.789)
+t:1000#list d
+
+/ solo: 1000 per sec  bulk: 10000 
+\t do[1000;pub[c;s;d]];req[c;s;()]
+\t do[1000;req[c;s;d]]
+\t req[c;s;t]
+
