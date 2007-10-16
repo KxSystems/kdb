b96ad5a6d65be9d2fcfa8ade6a8672d5c7b0a00d
svn <svn@b387b25f-291c-0410-8dc7-e51caacd5b47>
2007-10-16 20:19:06 +0000

    auto checkin

--- c/feed/rv.c
+++ c/feed/rv.c
@@ -6,24 +6,26 @@
 //s64>/usr/local/gcc-3.3.2/bin/gcc -G -m64 ../c/feed/rv.c -o rv.so -I/dat/tibrv/7.5.2/include -L/dat/tibrv/7.5.2/lib -ltibrv64
 #include"../c/k.h"
 #include"tibrv/tibrv.h"  //missing KC KM KD KU KV KT  3z,8s,9b 14..25  (22-none) 1msg 7opaque 26port 27addr 32encr 47xml
-extern V*malloc(),free();extern L memcpy(),strlen();
+extern V*malloc(),free();
 ZI tr(I t){R t==24?8:t/2-3;}ZJ o=10957*86400,j9=(J)1e9;Z tibrvMsgField f;Z tibrv_status s;
 ZI rt[]={0,9,0,0,14,16,18,20,24,25,8,8,0,0,0, 3};typedef tibrvMsg M;ZC inbox[99];
 ZI nt[]={0,4,0,0, 1, 2, 4, 8, 4, 8,0,0,0,0,0,12};ZV md(M m){tibrvMsg_Destroy(m);}
 ZK qf(){K x;I t=f.type;R t==3?kz((j9*(f.data.date.sec-o)+f.data.date.nsec)/8.64e13):
  t==8?kp((S)f.data.str):t==9?kb(f.data.boolean):t<14||(t>25&&t<34)||t>45||t==22?(x=ka(101),xi=0,x):
  t<26?(x=ka(-tr(t)),xj=f.data.i64,x):(x=ktn(tr(t-20),f.count),memcpy(xG,f.data.array,xn*f.size),x);}
-ZV fq(K x){ZS s;I a=xt<0,t=a?-xt:xt;J j;f.count=1,f.size=nt[t],f.type=rt[t];SW(xt){CS(-KB,f.data.boolean=xg)
+ZI fq(K x){ZS s;I a=xt<0,t=a?-xt:xt;J j;f.count=1,f.size=nt[t],f.type=rt[t];SW(xt){CS(-KB,f.data.boolean=xg)
  CS(-KZ,j=8.64e13*xf;f.data.date.sec=j/j9+o;f.data.date.nsec=j%j9)CS(-KS,f.size=strlen(f.data.str=xs)+1)
  CS(KC,if(s)free(s);f.data.str=s=(S)malloc(f.size=xn+1);memcpy(s,xG,xn);s[xn]=0)
- CD:if(a)f.data.i64=xj;else f.count=xn,f.type+=20,f.data.array=xG;}}
+ CD:U(t>3&&t<10)if(a)f.data.i64=xj;else f.count=xn,f.type+=20,f.data.array=xG;}R 1;}
 Z K qm(M m){S s;K x,y;I n;tibrvMsg_GetNumFields(m,&n),x=ktn(KS,n),y=ktn(0,n);
  DO(n,tibrvMsg_GetFieldByIndex(m,&f,i);s=(S)f.name;xS[i]=s?ss(s):0;kK(y)[i]=f.type==1?qm(f.data.msg):qf(f))
  R y=k(0,"k)::'",y,(K)0),xn&&*xS?xD(x,y):(r0(x),y);}
-Z M mq(K x){S s;K y,z;I j;M m,t;tibrvMsg_Create(&m);if(y=xt==XD?*xK:0)x=xK[1];if(r1(x),j=0<xt)x=k(0,"k)*:'(::),(;0)'",x,(K)0);if(!xt)
- DO(xn-j,z=xK[i+j];s=y?kS(y)[i]:0;if(z->t==XD){tibrvMsg_AddMsg(m,s,t=mq(z));md(t);}else{f.name=s;fq(z);tibrvMsg_AddField(m,&f);})R r0(x),m;}
-ZK snd(I i,S s,K x){M m=mq(x),r;tibrvMsg_SetSendSubject(m,s);P(i<0,(tibrvTransport_Send(-i,m),md(m),(K)0))
+Z M mq(K x){S s;K y,z;I j;M m,t;if(y=xt==XD?*xK:0){U(y->t==KS)x=xK[1];}if(r1(x),j=0<xt)x=k(0,"k)*:'(::),(;0)'",x,(K)0);
+ U(!xt)tibrvMsg_Create(&m);DO(xn-j,z=xK[i+j];s=y?kS(y)[i]:0;
+  if(z->t==XD){U(t=mq(z))tibrvMsg_AddMsg(m,s,t);md(t);}else{f.name=s;U(fq(z))tibrvMsg_AddField(m,&f);})R r0(x),m;}
+ZK snd(I i,S s,K x){M m=mq(x),r;P(!m,krr("type"))tibrvMsg_SetSendSubject(m,s);P(i<0,(tibrvTransport_Send(-i,m),md(m),(K)0))
  R tibrvMsg_SetReplySubject(m,inbox),tibrvTransport_SendRequest(i,m,&r,-1),md(m),x=qm(r),md(r),x;}
+
 ZV u(tibrvEvent e,M m,V*c){S s,r;K x;tibrvMsg_GetSendSubject(m,(const char**)&s),tibrvMsg_GetReplySubject(m,(const char**)&r);
  x=k(0,r?"rep":"upd",ks(ss(s)),qm(m),(K)0);if(xt==-128)O("error: %s\n",xs);if(r)snd(-(I)(L)c,r,x);r0(x);}
 #define Q(x) P((s=x),krr((S)tibrvStatus_GetText(s)))
--- c/feed/rv.q
+++ c/feed/rv.q
@@ -1,20 +1,22 @@
+/ publish dict(or list) of "bghijefzCGHIJEF" or dict
+
 con:`rv 2:(`con;1)
 sub:`rv 2:(`sub;2)
 req:`rv 2:(`req;3)
 pub:{req[neg x;y;z]}
 
-
 c:con```;s:`asd
-
 
 /server
 upd:{0N!(x;y)};rep:{y};sub[c;s]
-
+pub[c;s]2 3
 \
 
+
 /client
-d:`b`g`h`i`j`e`f`s`z!(1b;0x23;23h;23;23j;2.3e;2.3;"qwe";2000.01.02T12:34:56.789)
-t:1000#list d
+d:`b`g`h`i`j`e`f`z`C!(1b;0x23;23h;23;23j;2.3e;2.3;2000.01.02T12:34:56.789;"qwe")
+t:1000#enlist d
+
 
 / solo: 1000 per sec  bulk: 10000 
 \t do[1000;pub[c;s;d]];req[c;s;()]
