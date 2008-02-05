94554501cb1a66fd5fbd861171dfc0ee89a82c0b
svn <svn@b387b25f-291c-0410-8dc7-e51caacd5b47>
2008-02-05 16:19:07 +0000

    auto checkin

--- c/feed/ssl.c
+++ c/feed/ssl.c
@@ -1,27 +1,27 @@
+//2008.02.05 dispatch
 //cl /LD ..\c\feed\ssl.c ssl.def q.lib \ssl\ssl4w32.lib
-//gcc -shared -fPIC ../c/ssl.c -o ssl.so libssl.a
+//gcc -m32 -shared -fPIC ../c/feed/ssl.c -o ssl.so libssl.a
 //usr/local/gcc-3.3.2/bin/gcc -G -fPIC ../c/ssl.c -o ssl.so libssl.so
 //gcc -G -fPIC ../c/feed/ssl.c -o ssl.so /apps/reuters/ssl/lib/sun4_5.5/libssl.so.4
 #include"ssl.h"
 #include"../c/k.h"
-#define Q(x,s) if((x)<0){O("sslerror: %s\n",s);R krr(s);}
+#define Q(x,s) if((x)<0){O("sslerror: %s\n",s);R s;}
 ZI c=-1,d,f;ZK x,y;       K1(dispatch){sslDispatchEvent(SSL_ALL_CHANNEL,SSL_EXHAUST_READ);R 0;}
 ZK d0(I d){if(f)x=ktn(KS,0),y=ktn(0,0);else x=ktn(0,0);dispatch(0);P(xn,k(0,"f",x,y,(K)0))if(y)r0(y);R x;}
-ZK con(){Q(sslGetProperty(c,SSL_OPT_CHANNEL_FD,&d),"getfd")sd1(d,d0);R ktn(0,0);}
+ZS con(){Q(sslGetProperty(c,SSL_OPT_CHANNEL_FD,&d),"getfd")sd1(d,d0);R 0;}
 #define SSL(f,x) Z SSL_EVENT_RETCODE f(I c,SSL_EVENT_TYPE t,SSL_EVENT_INFO*e,void*v){x;R SSL_ER_EVENT_HANDLE_OK;}
 SSL(upd,{S p=e->ItemImage.Data;I n=e->ItemImage.DataLength;
  if(f){js(&x,ss(e->ItemImage.ItemName));jk(&y,kpn(p+1,n-2));}  //"I\037\036"0:
  else{DO(n,if(p[i]==30)p[i]='\n')p[n-1]='\n';jk(&x,kpn(p,n));}})
-
 SSL(stt,r0(k(0,"stt",ks(ss(e->ItemStatus.ItemName)),ks(ss(e->ItemStatus.Text)),ki(t),(K)0)))
 SSL(dis,r0(k(0,"dis[]",(K)0));sd0(d))SSL(rec,con();r0(k(0,"rec[]",(K)0)))
-
-ZK ini(S s){P(c!=-1,0)Q(sslInit(SSL_VERSION_NO),"sslinit")Q(c=sslSnkMount(s,0),"snkmount")
+ZS ini(S s){P(c!=-1,0)Q(sslInit(SSL_VERSION_NO),"sslinit")Q(c=sslSnkMount(s,0),"snkmount")
  sslRegisterClassCallBack(c,SSL_EC_ITEM_STATUS,stt,0);sslRegisterCallBack(c,SSL_ET_SESSION_DISCONNECTED,dis,0);
  sslRegisterClassCallBack(c,SSL_EC_DATA,upd,0);sslRegisterCallBack(c,SSL_ET_SESSION_RECONNECTED,rec,0);R con();}
-K1(init){f=xt==KC;R!f&&xt!=-KS?krr("type"):ini(f?sn(xC,xn):xs);}
-K2( ssub){I v=y->t==KS;P(xt!=-KS||!v&&y->t!=-KS,krr("type"))U(ini(""))DO(v?y->n:1,Q(sslSnkOpen(c,xs,v?kS(y)[i]:y->s,0,0),"open"))R r1(y);}
-K2(ussub){I v=y->t==KS;P(xt!=-KS||!v&&y->t!=-KS,krr("type"))U(ini(""))DO(v?y->n:1,Q(sslSnkClose(c,xs,v?kS(y)[i]:y->s),"close"))  R r1(y);}
+#define Q1(x,s) P(x,krr(s))
+K1(init){f=xt==KC;Q1(!f&&xt!=-KS,"type")Q1(ini(f?sn(xC,xn):xs),"init")R 0;}
+K2( ssub){I v=y->t==KS;S s;Q1(xt!=-KS||!v&&y->t!=-KS,"type")Q1(ini(""),"init")DO(v?y->n:1,Q1(sslSnkOpen(c,xs,s=v?kS(y)[i]:y->s,0,0),s))R r1(y);}
+K2(ussub){I v=y->t==KS;S s;Q1(xt!=-KS||!v&&y->t!=-KS,"type")Q1(ini(""),"init")DO(v?y->n:1,Q1(sslSnkClose(c,xs,s=v?kS(y)[i]:y->s),s))   R r1(y);}
 K  sub(K y){K x=ks(ss("IDN_SELECTFEED"));R y= ssub(x,y),r0(x),y;}
 K usub(K y){K x=ks(ss("IDN_SELECTFEED"));R y=ussub(x,y),r0(x),y;}
 
