903aebecbe5c9714f930d24026703a300e762b89
svn <svn@b387b25f-291c-0410-8dc7-e51caacd5b47>
2012-05-29 13:19:09 +0000

    auto checkin

--- c/c.cs
+++ c/c.cs
@@ -1,3 +1,4 @@
+//2012.05.29 for use with kdb+v3.0, changed handshake and added Guid
 //2012.01.26 refactored clamp into clampDT, for Date.DateTime()
 //2012.01.25 rz() clamp datetime to valid range
 //2010.11.17 Block sending new timetypes to version of kdb+ prior to v2.6 (use prior release of c.cs for older kdb+ versions)
@@ -34,14 +35,14 @@ public static System.Text.Encoding e=System.Text.Encoding.ASCII;
 byte[]b,B;int j,J;bool a,v6;Stream s;public new void Close(){s.Close();base.Close();}
 public c(string h,int p):this(h,p,Environment.UserName){}
 public c(string h,int p,string u):this(h,p,u,DefaultMaxBufferSize){}
-public c(string h,int p,string u,int maxBufferSize){_maxBufferSize=maxBufferSize;Connect(h,p);s=this.GetStream();B=new byte[2+u.Length];J=0;w(u+"\x1");s.Write(B,0,J);if(1!=s.Read(B,0,1)){B=new byte[1+u.Length];Connect(h,p);s=this.GetStream();J=0;w(u);s.Write(B,0,J);if(1!=s.Read(B,0,1))throw new KException("access");}v6=B[0]==1;}
+public c(string h,int p,string u,int maxBufferSize){_maxBufferSize=maxBufferSize;Connect(h,p);s=this.GetStream();B=new byte[2+u.Length];J=0;w(u+"\x2");s.Write(B,0,J);if(1!=s.Read(B,0,1)){B=new byte[1+u.Length];Connect(h,p);s=this.GetStream();J=0;w(u);s.Write(B,0,J);if(1!=s.Read(B,0,1))throw new KException("access");}v6=B[0]>=1;}
 static int ns(string s){int i=s.IndexOf('\0');i=-1<i?i:s.Length;return e.GetBytes(s.Substring(0,i)).Length;}
 static TimeSpan t(){return DateTime.Now.TimeOfDay;}static TimeSpan v;static void tm(){TimeSpan u=v;v=t();O(v-u);}
 static void O(object x){Console.WriteLine(x);}static string i2(int i){return String.Format("{0:00}",i);}
 static int ni=Int32.MinValue;static long nj=Int64.MinValue,o=(long)8.64e11*730119;static double nf=Double.NaN;
-static object[]NU={null,false,null,null,(byte)0,Int16.MinValue,ni,nj,(Single)nf,nf,' ',"",new DateTime(0),
+static object[]NU={null,false,new Guid(),null,(byte)0,Int16.MinValue,ni,nj,(Single)nf,nf,' ',"",new DateTime(0),
  new Month(ni),new Date(ni),new DateTime(0),new KTimespan(nj),new Minute(ni),new Second(ni),new TimeSpan(ni)};
-static object NULL(char c){return NU[" b  xhijefcspmdznuvt".IndexOf(c)];}
+static object NULL(char c){return NU[" bg xhijefcspmdznuvt".IndexOf(c)];}
 public static object NULL(Type t){for(int i=0;i<NU.Length;i++)if(NU[i]!=null&&t==NU[i].GetType())return NU[i];return null;}
 public static bool qn(object x){int t=-c.t(x);return t>4&&x.Equals(NU[t]);}
 private void u(){int n=0,r=0,f=0,s=8,p=s;short i=0;j=0;byte[]dst=new byte[ri()];int d=j;int[]aa=new int[256];
@@ -84,11 +85,11 @@ public class Flip{public string[]x;public object[]y;public Flip(Dict X){x=(strin
 public static Flip td(object X){if(t(X)==98)return(Flip)X;Dict d=(Dict)X;Flip a=(Flip)d.x,b=(Flip)d.y;int m=c.n(a.x),n=c.n(b.x);
  string[]x=new string[m+n];Array.Copy(a.x,0,x,0,m);Array.Copy(b.x,0,x,m,n);
  object[]y=new object[m+n];Array.Copy(a.y,0,y,0,m);Array.Copy(b.y,0,y,m,n);return new Flip(new Dict(x,y));}
-static int t(object x){return x is bool?-1:x is byte?-4:x is short?-5:x is int?-6:x is long?-7:x is float?-8:x is double?-9:x is char?-10:
+static int t(object x){return x is bool?-1:x is Guid?-2:x is byte?-4:x is short?-5:x is int?-6:x is long?-7:x is float?-8:x is double?-9:x is char?-10:
  x is string?-11:x is DateTime?-12:x is Month?-13:x is Date?-14:x is DateTime?-15:x is KTimespan?-16:x is Minute?-17:x is Second?-18:x is TimeSpan?-19:
- x is bool[]?1:x is byte[]?4:x is short[]?5:x is int[]?6:x is long[]?7:x is float[]?8:x is double[]?9:x is char[]?10:
+ x is bool[]?1:x is Guid[]?2:x is byte[]?4:x is short[]?5:x is int[]?6:x is long[]?7:x is float[]?8:x is double[]?9:x is char[]?10:
  x is DateTime[]?12:x is DateTime[]?15:x is KTimespan[]?16:x is TimeSpan[]?19:x is Flip?98:x is Dict?99:0;}
-static int[]nt={0,1,0,0,1,2,4,8,4,8,1,0,8,4,4,8,8,4,4,4};// x.GetType().IsArray
+static int[]nt={0,1,16,0,1,2,4,8,4,8,1,0,8,4,4,8,8,4,4,4};// x.GetType().IsArray
 static int n(object x){return x is Dict?n(((Dict)x).x):x is Flip?n(((Flip)x).y[0]):x is char[]?e.GetBytes((char[])x).Length:((Array)x).Length;}
 static int nx(object x){int i=0,n,t=c.t(x),j;if(t==99)return 1+nx(((Dict)x).x)+nx(((Dict)x).y);if(t==98)return 3+nx(((Flip)x).x)+nx(((Flip)x).y);
  if(t<0)return t==-11?2+ns((string)x):1+nt[-t];j=6;n=c.n(x);if(t==0)for(;i<n;++i)j+=nx(((object[])x)[i]);else j+=n*nt[t];return j;}
@@ -97,6 +98,9 @@ public static object at(object x,int i){object r=((Array)x).GetValue(i);return q
 void w(bool x){B[J++]=(byte)(x?1:0);}bool rb(){return 1==b[j++];}void w(byte x){B[J++]=x;}byte rx(){return b[j++];}
 void w(short h){B[J++]=(byte)h;B[J++]=(byte)(h>>8);}short rh(){int x=b[j++],y=b[j++];return(short)(a?x&0xff|y<<8:x<<8|y&0xff);}
 void w(int i){w((short)i);w((short)(i>>16));}int ri(){int x=rh(),y=rh();return a?x&0xffff|y<<16:x<<16|y&0xffff;}
+private byte[]gip={3,2,1,0,5,4,7,6,8,9,10,11,12,13,14,15};
+void w(Guid g){byte[]b=g.ToByteArray();for(int i=0;i<b.Length;i++)w(b[gip[i]]);}
+Guid rg(){bool oa=a;a=false;int i=ri();short h1=rh(),h2=rh();a=oa;byte[]b=new byte[8];for(int j=0;j<8;j++)b[j]=rx();return new Guid(i,h1,h2,b);}
 void w(long j){w((int)j);w((int)(j>>32));}long rj(){int x=ri(),y=ri();return a?x&0xffffffffL|(long)y<<32:(long)x<<32|y&0xffffffffL;}
 void w(float e){byte[]b=BitConverter.GetBytes(e);foreach(byte i in b)w(i);}float re(){byte c;float e;
  if(!a){c=b[j];b[j]=b[j+3];b[j+3]=c;c=b[j+1];b[j+1]=b[j+2];b[j+2]=c;}e=BitConverter.ToSingle(b,j);j+=4;return e;}
@@ -111,7 +115,8 @@ void w(DateTime p){if(!v6)throw new KException("Timestamp not valid pre kdb+2.6"
 DateTime rz(){double f=rf();return Double.IsInfinity(f)?(f<0?za:zw):new DateTime(qn(f)?0:clampDT(10000*(long)Math.Round(8.64e7*f)+o));}
 void w(KTimespan t){w(qn(t)?nj:(t.t.Ticks*100));} KTimespan rn(){return new KTimespan(rj());}
 DateTime rp(){long j=rj(),d=j<0?(j+1)/100-1:j/100;DateTime p=new DateTime(j==nj?0:o+d);return p;}
-void w(object x){int t=c.t(x);w((byte)t);if(t<0)switch(t){case-1:w((bool)x);return;case-4:w((byte)x);return;
+void w(object x){int t=c.t(x);w((byte)t);if(t<0)switch(t){case-1:w((bool)x);return;
+ case-2:w((Guid)x);return;case-4:w((byte)x);return;
  case-5:w((short)x);return;case-6:w((int)x);return;case-7:w((long)x);return;case-8:w((float)x);return;case-9:w((double)x);return;
  case-10:w((char)x);return;case-11:w((string)x);return;case-12:w((DateTime)x);return;case-13:w((Month)x);return;
  case-14:w((Date)x);return;case-15:w((DateTime)x);return;case-16:w((KTimespan)x);return;case-17:w((Minute)x);return;case-18:w((Second)x);return;
@@ -119,6 +124,7 @@ void w(object x){int t=c.t(x);w((byte)t);if(t<0)switch(t){case-1:w((bool)x);retu
  if(t==99){Dict r=(Dict)x;w(r.x);w(r.y);return;}B[J++]=0;if(t==98){Flip r=(Flip)x;B[J++]=99;w(r.x);w(r.y);return;}
  w(c.n(x));switch(t){
   case 0:foreach(object o in(object[])x)w(o);return;           case 1:foreach(bool o in(bool[])x)w(o);return;
+  case 2:foreach(Guid o in(Guid[])x)w(o);return;
   case 4:foreach(byte o in(byte[])x)w(o);return;               case 5:foreach(short o in(short[])x)w(o);return;
   case 6:foreach(int o in(int[])x)w(o);return;                 case 7:foreach(long o in(long[])x)w(o);return;
   case 8:foreach(float o in(float[])x)w(o);return;             case 9:foreach(double o in(double[])x)w(o);return;
@@ -128,12 +134,13 @@ void w(object x){int t=c.t(x);w((byte)t);if(t<0)switch(t){case-1:w((bool)x);retu
   case 16:foreach(KTimespan o in(KTimespan[])x)w(o);return;    case 17:foreach(Minute o in(Minute[])x)w(o);return;
   case 18:foreach(Second o in(Second[])x)w(o);return;          case 19:foreach(TimeSpan o in(TimeSpan[])x)w(o);return;}
 }
-object r(){int i=0,n,t=(sbyte)b[j++];if(t<0)switch(t){case-1:return rb();case-4:return b[j++]; case-5:return rh();
+object r(){int i=0,n,t=(sbyte)b[j++];if(t<0)switch(t){case-1:return rb();case-2:return rg();case-4:return b[j++]; case-5:return rh();
  case-6:return ri();case-7:return rj();case-8:return re();case-9:return rf();case-10:return rc();case-11:return rs();
  case -12: return rp();case-13:return rm();case-14:return rd();case-15:return rz();case -16:return rn();case-17:return ru();
  case-18:return rv();case-19:return rt();}
  if(t>99){if(t==101&&b[j++]==0)return null;throw new KException("func");}if(t==99)return new Dict(r(),r());j++;if(t==98)return new Flip((Dict)r());n=ri();switch(t){
   case 0:object[]L=new object[n];for(;i<n;i++)L[i]=r();return L;        case 1:bool[]B=new bool[n];for(;i<n;i++)B[i]=rb();return B;
+  case 2:{Guid[]G=new Guid[n];for(;i<n;i++)G[i]=rg();return G;}
   case 4:byte[]G=new byte[n];for(;i<n;i++)G[i]=b[j++];return G;         case 5:short[]H=new short[n];for(;i<n;i++)H[i]=rh();return H;
   case 6:int[]I=new int[n];for(;i<n;i++)I[i]=ri();return I;             case 7:long[]J=new long[n];for(;i<n;i++)J[i]=rj();return J;
   case 8:float[]E=new float[n];for(;i<n;i++)E[i]=re();return E;         case 9:double[]F=new double[n];for(;i<n;i++)F[i]=rf();return F;
