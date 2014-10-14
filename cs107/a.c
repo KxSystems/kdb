a30fc10a477e85b07e21f3c229dfc979ee80e0ed
svn <svn@b387b25f-291c-0410-8dc7-e51caacd5b47>
2014-10-14 19:19:08 +0000

    auto checkin

--- cs107/a.c
+++ cs107/a.c
@@ -10,6 +10,6 @@ V g(I n,S*b){I m=0,mi=0,mj=1;N(n,J(n,I k=i!=j?f(b[i],b[j]):0;I(m<k)m=k,mi=i,mj=j
  S s=b[mi],t=b[mj];b[mi]=strcat(strcpy(malloc(strlen(s)+strlen(t)+1-m),s),t+m),b[mj]=b[n-1],free(s),free(t);}
 
 // run
-I main(I n,S*x){L m;S b[20000],s=map(&m,x[1]),t=s+m;A(s)
- F(n=0;s<t;++s){S r;I('{'==*s)r=s;I('}'==*s)b[n++]=strndup(s,s-r);}
+I main(I n,S*x){L m;S b[20000],s=map(&m,x[1]);A(s)
+ n=0;N(m-1,I h;I('{'==s[i])h=i+1;I('}'==s[i])b[n++]=strndup(s+h,i-h))
  W(1<n)g(n--,b);O("%s\n",*b),free(*b);}
