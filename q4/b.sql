0aafd08ebc45982bb6b7f792051c144a21f93176
svn <svn@b387b25f-291c-0410-8dc7-e51caacd5b47>
2015-03-11 23:19:05 +0000

    auto checkin

--- q4/b.sql
+++ q4/b.sql
@@ -2,20 +2,19 @@ drop table if exists S;create table S(sym char(4));
 insert into S select sym from trade group by sym order by count(*) desc fetch first 100 rows only;
 
 \timing on
-explain(analyze)select sym,last(bid) from quote where sym in(select sym from S)group by sym;
-explain(analyze)select sym,ex,max(price) from trade where sym in(select sym from S)group by sym,ex;
-explain(analyze)select sym,extract(hour from time),avg(size) from trade where sym in(select sym from S)group by sym,extract(hour from time);
+explain(analyze)select sym,last(bid) from quote natural join S group by sym;
+explain(analyze)select sym,ex,max(price) from trade natural join S group by sym,ex;
+explain(analyze)select sym,extract(hour from time),avg(size) from trade natural join S group by sym,extract(hour from time);
 
 \q
-
 
 --ETL
 drop table if exists trade;create table trade(sym char(4),time time,ex char(1),cond char(1),size int,price float);
 drop table if exists quote;create table quote(sym char(4),time time,ex char(1),bid float,bsize int,ask float,asize int,mode char(1));
 
 \timing on
-copy trade from '/a/tq/t.csv' delimiter ',' csv;create index ts on trade(sym);
-copy quote from '/a/tq/q.csv' delimiter ',' csv;create index qs on quote(sym);
+copy trade from 't.csv' delimiter ',' csv;create index ts on trade(sym);
+copy quote from 'q.csv' delimiter ',' csv;create index qs on quote(sym);
 analyze;
 
 CREATE OR REPLACE FUNCTION public.first_agg(anyelement,anyelement)RETURNS anyelement LANGUAGE sql IMMUTABLE STRICT AS $$ SELECT $1; $$;
