4b39e50ce153b0297faf8fe6334bc4c3e5d92f48
CharlieSkelton-Kx <CharlieSkelton-Kx@users.noreply.github.com>
2017-12-03 14:39:49 +0100

    added K ee(K)
    see http://code.kx.com/q/interfaces/capiref/#ee-error-string

--- c/c/k.h
+++ c/c/k.h
@@ -69,7 +69,7 @@ extern V m9();
 extern V m9(V);
 #endif
 extern I khpun(const S,I,const S,I),khpu(const S,I,const S),khp(const S,I),okx(K),ymd(I,I,I),dj(I);extern V r0(K),sd0(I),sd0x(I d,I f),kclose(I);extern S sn(S,I),ss(S);
-extern K ktj(I,J),ka(I),kb(I),kg(I),kh(I),ki(I),kj(J),ke(F),kf(F),kc(I),ks(S),kd(I),kz(F),kt(I),sd1(I,K(*)(I)),dl(V*f,I),
+extern K ee(K),ktj(I,J),ka(I),kb(I),kg(I),kh(I),ki(I),kj(J),ke(F),kf(F),kc(I),ks(S),kd(I),kz(F),kt(I),sd1(I,K(*)(I)),dl(V*f,I),
  knk(I,...),kp(S),ja(K*,V*),js(K*,S),jk(K*,K),jv(K*k,K),k(I,const S,...),xT(K),xD(K,K),ktd(K),r1(K),krr(const S),orr(const S),dot(K,K),b9(I,K),d9(K);
 #ifdef __cplusplus 
 }
