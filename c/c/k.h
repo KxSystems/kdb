cb39e13eb6b4aa62a7b89926d535cfe523d12257
CharlieSkelton-Kx <charlie@kx.com>
2018-06-04 20:56:04 +0200

    added new functions released in c libs 2018.06.04

--- c/c/k.h
+++ c/c/k.h
@@ -13,7 +13,7 @@ typedef struct{G g[16];}U;
 #define kU(x) ((U*)kG(x))
 #define xU ((U*)xG)
 extern K ku(U),knt(J,K),ktn(I,J),kpn(S,J);
-extern I setm(I);
+extern I setm(I),ver();
 #define DO(n,x)	{J i=0,_i=(n);for(;i<_i;++i){x;}}
 #else
 typedef struct k0{I r;H t,u;union{G g;H h;I i;J j;E e;F f;S s;struct k0*k;struct{I n;G G0[1];};};}*K;
@@ -68,9 +68,9 @@ extern V m9();
 #else
 extern V m9(V);
 #endif
-extern I khpun(const S,I,const S,I),khpu(const S,I,const S),khp(const S,I),okx(K),ymd(I,I,I),dj(I);extern V r0(K),sd0(I),sd0x(I d,I f),kclose(I);extern S sn(S,I),ss(S);
+extern I khpunc(S,I,S,I,I),khpun(const S,I,const S,I),khpu(const S,I,const S),khp(const S,I),okx(K),ymd(I,I,I),dj(I);extern V r0(K),sd0(I),sd0x(I d,I f),kclose(I);extern S sn(S,I),ss(S);
 extern K ee(K),ktj(I,J),ka(I),kb(I),kg(I),kh(I),ki(I),kj(J),ke(F),kf(F),kc(I),ks(S),kd(I),kz(F),kt(I),sd1(I,K(*)(I)),dl(V*f,I),
- knk(I,...),kp(S),ja(K*,V*),js(K*,S),jk(K*,K),jv(K*k,K),k(I,const S,...),xT(K),xD(K,K),ktd(K),r1(K),krr(const S),orr(const S),dot(K,K),b9(I,K),d9(K);
+ knk(I,...),kp(S),ja(K*,V*),js(K*,S),jk(K*,K),jv(K*k,K),k(I,const S,...),xT(K),xD(K,K),ktd(K),r1(K),krr(const S),orr(const S),dot(K,K),b9(I,K),d9(K),sslInfo(K x);
 #ifdef __cplusplus 
 }
 #endif
